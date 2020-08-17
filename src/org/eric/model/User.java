ddee1e627a75a7b428f9bdde8283a3bc1fb0ada6
Eric <ericxjd@yahoo.ca>
2020-08-16 21:24:48 -0400

    Added constructors

--- src/org/eric/model/User.java
+++ src/org/eric/model/User.java
@@ -3,49 +3,43 @@ package org.eric.model;
 public class User {
     private String name;
     private int age;
+    
+    
+    
 
-    /**
-     * @return the name
-     */
+
+    
     public String getName() {
         return name;
     }
 
-    /**
-     * @param name the name to set
-     */
     public void setName(String name) {
         this.name = name;
     }
 
-
-
-    
-    
-    /**
-     * @return the age
-     */
     public int getAge() {
         return age;
     }
 
-    /**
-     * @param age the age to set
-     */
     public void setAge(int age) {
         this.age = age;
     }
 
-    /* (non-Javadoc)
-     * @see java.lang.Object#toString()
-     */
     @Override
     public String toString() {
         return "User [name=" + name + ", age=" + age + "]";
     }
-    
-    
 
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    
+    public User() {
+    }
+    
     
     
 }
